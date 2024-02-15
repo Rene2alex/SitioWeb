678ca8eee3a07803b3dbb38c776c870683916999
Rene alejandro Hernandez Ramirez <Alexramires2022@outlook.es>
2024-02-14 19:23:37 -0600

    SitioWeb

---
+++ template/Cabesera.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<link rel="stylesheet" href="./css/style.css">
+
+<i class="bi bi-pencil"></i>
+<title>Navegador a la Izquierda</title>
+
+</head>
+<body>
+
+<!-- Barra de navegación -->
+<nav>
+  <ul >
+  <br/>
+    <li><a href="Index.php">Cliente</a></li>
+    <li><a href="Facturas.php">Pagos</a></li>
+    <li><a href="#">Reportes</a></li>
+    <li><a href="Login.php">login</a></li>
+    <li><a href="AgregarF.php">AGREGAR FACTURA</a></li>
+ 
+  
+  </ul>
+</nav>
